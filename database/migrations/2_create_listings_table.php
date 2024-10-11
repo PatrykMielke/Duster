2cd50800cd735757f9205bd886f9a45b29baa4aa
Patryk Mielke <163316678+PatrykMielke@users.noreply.github.com>
2024-10-11 19:10:47 +0200

    poprawione

--- database/migrations/2_create_listings_table.php
+++ database/migrations/2_create_listings_table.php
@@ -7,6 +7,8 @@ use App\Models\Color;
 use App\Models\Status;
 use App\Models\Category;
 use App\Models\Condition;
+use App\Models\ListingColor;
+use App\Models\ListingMaterial;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -28,7 +30,8 @@ return new class extends Migration
             $table->foreignIdFor(Category::class)->constrained();
             $table->foreignIdFor(Size::class)->constrained();
             $table->foreignIdFor(Brand::class)->constrained();
-            $table->foreignIdFor(Color::class)->constrained();
+            $table->foreignIdFor(ListingColor::class)->constrained();
+            $table->foreignIdFor(ListingMaterial::class)->constrained();
             $table->foreignIdFor(Condition::class)->constrained();
             $table->foreignIdFor(Status::class)->constrained();
 
