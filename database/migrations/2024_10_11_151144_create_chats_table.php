e9566dd4c4d71678993b5da4cb51f70d37ee1ce3
Igor Igor <iigor500@gmail.com>
2024-10-11 17:35:52 +0200

    dupa

---
+++ database/migrations/2024_10_11_151144_create_chats_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('listing_id');
+            $table->foreignId(column: 'sender');
+            $table->foreignId(column: 'reciever');
+            $table->string(column: 'message');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+};
