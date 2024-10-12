5d2036fa70836a1b5f70da164a7cd523d01128b7
Igor Igor <iigor500@gmail.com>
2024-10-12 17:11:50 +0200

    ffffff

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,98 +2,44 @@
 
 namespace Database\Seeders;
 
-<<<<<<< HEAD
+use App\Models\User;
 use App\Models\Brand;
-use App\Models\Condition;
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\DeliveryMethods;
-use App\Models\Material;
-=======
-use App\Models\Category;
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Color;
+use App\Models\Status;
+use App\Models\Category;
+use App\Models\Material;
+use App\Models\Condition;
 use App\Models\PaymentMethods;
->>>>>>> bd08b9d309a6c05282f5b2d710534382b4369383
+use App\Models\DeliveryMethods;
 use Illuminate\Database\Seeder;
-use Database\Seeders\MaterialSeeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    private static $rodzajeUbran = [
-        'Koszulka',
-        'Spodnie',
-        'Kurtka',
-        'Bluza',
-        'Sukienka',
-        'Spódnica',
-        'Sweter',
-        'Marynarka',
-        'Szorty',
-        'Garnitur',
-        'T-shirt',
-        'Jeansy',
-        'Kamizelka',
-        'Golf',
-        'Płaszcz',
-        'Kardigan',
-        'Trencz',
-        'Legginsy',
-        'Tunika',
-        'Body',
-        'Kombinezon',
-        'Szlafrok',
-        'Piżama',
-        'Fartuch',
-        'Koszula',
-        'Bezrękawnik',
-        'Rękawiczki',
-        'Kapelusz',
-        'Czapka',
-        'Chusta',
-        'Beret',
-        'Pasek',
-        'Okulary przeciwsłoneczne',
-        'Torebka',
-        'Plecak',
-        'Portfel',
-        'Zegarek',
-        'Parasol',
-        'Szal',
-        'Naszyjnik',
-        'Bransoletka',
-        'Kolczyki',
-        'Pierścionek',
-        'Broszka',
-        'Łańcuszek',
-        'Spinka do włosów',
-        'Zegarek na rękę'
-    ];
-
     public function run(): void
     {
-<<<<<<< HEAD
         Brand::truncate();
         Condition::truncate();
         DeliveryMethods::truncate();
         Material::truncate();
-
+        Color::truncate();
+        PaymentMethods::truncate();
+        Category::truncate();
+        Status::truncate();
+        User::truncate();
 
         $this->call(class: BrandSeeder::class);
         $this->call(class: ConditionSeeder::class);
         $this->call(class: DeliveryMethodSeeder::class);
         $this->call(class: MaterialSeeder::class);
 
-=======
         User::factory(20)->create();
         $this->call(ColorSeeder::class);
         $this->call(PaymentMethodsSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(StatusSeeder::class);
->>>>>>> bd08b9d309a6c05282f5b2d710534382b4369383
     }
 }
