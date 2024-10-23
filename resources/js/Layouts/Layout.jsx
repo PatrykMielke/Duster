66f4cd7987027dbec9de607a9512e2ceba5c01a6
Patryk Mielke <163316678+PatrykMielke@users.noreply.github.com>
2024-10-23 14:00:47 +0200

    fds

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../Components/Navbar";
+import Navbar from "../Pages/Misc/Navbar/Navbar";
 // import Navbar from "../Pages/Misc/Navbar/Navbar";
 
 function classNames(...classes) {
