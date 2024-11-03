7207fdbb76efdb90535a0d90dd269389100a8839
Patryk Mielke <163316678+PatrykMielke@users.noreply.github.com>
2024-11-03 15:48:58 +0100

    x

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import react from "@vitejs/plugin-react";
+import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
     plugins: [
@@ -9,6 +10,7 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        compression(),
     ],
     optimizeDeps: {
         exclude: ["js-big-decimal"],
