b5af97d1ded560ee73ba1ef2f697b03b09e737df
Patryk Mielke <163316678+PatrykMielke@users.noreply.github.com>
2024-11-13 14:55:04 +0100

    rejestracja

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -41,12 +41,13 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'is_active' => true,
         ]);
 
         event(new Registered($user));
 
         Auth::login($user);
 
-        return redirect(route('HomePage', absolute: false));
+        return redirect(route('index', absolute: false));
     }
 }
