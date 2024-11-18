3d218dc9190e886480c911d7ee5e7243c26124a9
Igor Igor <iigor500@gmail.com>
2024-11-18 16:38:08 +0100

    chechout overfill grid

--- resources/js/Pages/Listing/Checkout/Checkout.jsx
+++ resources/js/Pages/Listing/Checkout/Checkout.jsx
@@ -59,20 +59,12 @@ export default function CheckoutForm({ listing, auth, deliveryMethods, paymentMe
         formData.append('buyer_id', auth.user.id);
 
 
-
-
         post(route("orders.store"), {
             data: formData,
             onFinish: () => reset(),
             forceFormData: true,
         });
-
-
-
-
     };
-
-
 
 
     const validateCurrentStep = () => {
@@ -80,19 +72,16 @@ export default function CheckoutForm({ listing, auth, deliveryMethods, paymentMe
         return true;
     };
 
+
     const handleStepChange = (stepId) => {
         if (stepId < currentStep || validateCurrentStep()) {
             setCurrentStep(stepId);
         }
     };
 
-
-
     return (
         <Layout>
             <div className="">
-
-
 
                 {/* Sekcja Formularza */}
                 <form onSubmit={formSubmit}>
@@ -103,7 +92,6 @@ export default function CheckoutForm({ listing, auth, deliveryMethods, paymentMe
 
                             <div className="max-w-6xl mx-auto bg-zinc-100 rounded-lg shadow-lg font-sans p-4 space-y-4">
                                 <h2 className="text-xl font-semibold text-gray-700 mb-4">Adres dostawy</h2>
-
 
                                 <input
                                     type="text"
@@ -160,14 +148,13 @@ export default function CheckoutForm({ listing, auth, deliveryMethods, paymentMe
 
 
 
-
                             <div className="max-w-6xl mx-auto p-4 bg-zinc-100 rounded-lg shadow-lg font-sans">
                                 <h2 className="text-xl font-semibold text-gray-700 mb-4">Sposób dostawy</h2>
-                                <div className="flex gap-4 mb-3">
+                                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                                     {deliveryMethods.map((method) => (
                                         <label
                                             key={method.name}
-                                            className="flex items-center w-full p-4 border border-gray-300 rounded-md hover:bg-gray-100 cursor-pointer"
+                                            className="flex items-center p-4 border border-gray-300 rounded-md hover:bg-gray-100 cursor-pointer"
                                         >
                                             <input
                                                 type="radio"
@@ -175,11 +162,11 @@ export default function CheckoutForm({ listing, auth, deliveryMethods, paymentMe
                                                 value={method.id}
                                                 checked={data.delivery_method === method.id}
                                                 onChange={() => setData("delivery_method", method.id)}
-                                                className="mr-3"
+                                                className="mr-3 shrink-0"
                                             />
-                                            <div>
+                                            <div className="truncate">
                                                 <span className="font-medium">{method.name}</span>
-                                                <p className="text-sm text-gray-500">{method.price}zł</p>
+                                                <p className="text-sm text-gray-500 truncate">{method.price}zł</p>
                                             </div>
                                         </label>
                                     ))}
@@ -193,7 +180,7 @@ export default function CheckoutForm({ listing, auth, deliveryMethods, paymentMe
 
                                 <div className="max-w-6xl mx-auto p-4 bg-zinc-100 rounded-lg shadow-lg font-sans">
                                     <h2 className="text-xl font-semibold text-gray-700 mb-4">Sposób płatności</h2>
-                                    <div className="flex gap-4 mb-3">
+                                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                                         {paymentMethods.map((method) => (
                                             <label
                                                 key={method.name}
